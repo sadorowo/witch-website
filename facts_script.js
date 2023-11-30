c9022e69883ab522067e3bbb0e5ae56aab6d3e5d
sadorowo <97941280+sadorowo@users.noreply.github.com>
2023-11-30 14:02:47 +0000

    update: new slider system

--- facts_script.js
+++ facts_script.js
@@ -1,7 +1,7 @@
-const [leftArrow, rightArrow] = ['.arrow-button[data-left]', '.arrow-button[data-right]'].map(s => document.querySelector(s));
-const fact = document.getElementById('fact');
+const $facts = document.getElementById('facts');
+const $fact = document.getElementById('fact');
 
-const facts = [
+const factList = [
     'Czarownice były często oskarżane o praktykowanie magii i czarów, które miały rzekomo wpływać na ludzi, zwierzęta i rośliny.',
     'W XV-XVII wieku w Europie miało miejsce około 40 000-60 000 egzekucji osób oskarżonych o czary, z których większość stanowiły kobiety.',
     'Jednym z najbardziej znanych procesów czarownic w historii były procesy salemzkie w 1692 roku w kolonii Massachusetts, gdzie oskarżono i skazano na śmierć 20 osób.',
@@ -14,21 +14,43 @@ const facts = [
     'Ostatnią spaloną na stosie „czarownicą” w Europie była Barbara Zdunk.'
 ]
 
+let [touchStartX, touchEndX] = [-1, -1];
 let index = 0;
 
 function previous() {
-    if (index - 1 < 0) index = facts.length;
+    if (index - 1 < 0) index = factList.length;
 
     index--;
-    fact.textContent = facts[index];
+    $fact.textContent = factList[index];
 }
 
 function next() {
-    if (index + 1 > facts.length - 1) index = -1;
+    if (index + 1 > factList.length - 1) index = -1;
 
     index++;
-    fact.textContent = facts[index];
+    $fact.textContent = factList[index];
 }
 
-leftArrow.addEventListener('click', previous);
-rightArrow.addEventListener('click', next);
+function checkDirection() {
+    if (touchEndX < touchStartX) previous()
+    if (touchEndX > touchStartX) next()
+}
+
+document.addEventListener('touchstart', e => {
+    touchStartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+    touchEndX = e.changedTouches[0].screenX
+    checkDirection()
+})
+
+$facts.addEventListener('wheel', e => {
+    if (e.deltaY > 0) {
+        index++;
+        next()
+    } else {
+        index--;
+        previous()
+    }
+})
